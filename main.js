1b1de67158a4a316d5520b8a57e2d748824970e8
Nathan Zeager <nathanzeager@hotmail.com>
2022-08-02 19:28:29 -0400

    Displaying items for fixed url, added css

--- main.js
+++ main.js
@@ -14,8 +14,36 @@ fetch(searchUrl, {
     // data is whatever the above code returns, in this case response.json()
     .then(data => {
         let songs = data.results;
-        // console.log(songs);
-        for(let song of songs) {
-
-        }
+        console.log(songs);
+        showTracks(songs);
     });
+
+function showTracks (trackArray) {
+    for(let track of trackArray) {
+        let songDiv = document.createElement("div");
+        songDiv.classList.add("song");
+        
+        //image
+        let imageDiv = document.createElement("div");
+        imageDiv.classList.add("songImg");
+        let picture = document.createElement("img");
+        picture.src = track.artworkUrl100;
+        imageDiv.appendChild(picture);
+        songDiv.appendChild(imageDiv);
+
+        //track
+        let trackDiv = document.createElement("div");
+        trackDiv.classList.add("trackName");
+        trackDiv.innerText = track.trackName;
+        songDiv.appendChild(trackDiv);
+
+        //artist
+        let artistDiv = document.createElement("div");
+        artistDiv.classList.add("artist");
+        artistDiv.innerText = track.artistName;
+        songDiv.appendChild(artistDiv);
+
+        // append songDiv to results
+        resultsDiv.appendChild(songDiv);
+    }
+}
