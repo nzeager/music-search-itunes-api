2c3c49998d9233dfc7419594d4969f3b0c2bb887
Nathan Zeager <nathanzeager@hotmail.com>
2022-08-03 11:12:42 -0400

    Search functionality implemented for initial search

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 // console.log('connected!');
 
-let resultsDiv = document.querySelector('#results');
+const resultsDiv = document.querySelector('#results');
 // console.log(resultsDiv);
 
-let searchUrl = 'https://proxy-itunes-api.glitch.me/search?term=jack+johnson';
+let searchUrl = 'https://proxy-itunes-api.glitch.me/search?term=';
 
-fetch(searchUrl, {
-    method: 'GET',
-    headers: { 'Content-Type': 'text/javascript; charset=utf-8'}
-    })
-    // response is whatever the function returns
-    .then(response => response.json())
-    // data is whatever the above code returns, in this case response.json()
-    .then(data => {
-        let songs = data.results;
-        console.log(songs);
-        showTracks(songs);
-    });
+let searchForm = document.querySelector('#search-form');
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let searchBox = document.querySelector('#search-box');
+    let urlEnd = searchBox.value.replaceAll(' ', '+');
+    console.log(urlEnd);
+    searchUrl = `${searchUrl}${urlEnd}`;
+    getSearchResults(searchUrl);
+})
+
+function getSearchResults(url) {
+    fetch(url, {
+        method: 'GET',
+        headers: { 'Content-Type': 'text/javascript; charset=utf-8'}
+        })
+        // response is whatever the function returns
+        .then(response => response.json())
+        // data is whatever the above code returns, in this case response.json()
+        .then(data => {
+            let songs = data.results;
+            console.log(songs);
+            showTracks(songs);
+        })
+}
 
 function showTracks (trackArray) {
     for(let track of trackArray) {
